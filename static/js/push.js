bd80c779062a3b8b361c57c3766e47654aecaaf0
liranmotol <liranmotola@gmail.com>
2016-02-27 01:16:43 +0200

    converted to confrence app

--- static/js/push.js
+++
@@ -1,9 +0,0 @@
-angular.module('conference.push', ['conference.config'])
-
-    .factory('Notification', function ($http, PUSH_SERVER_URL) {
-        return {
-            push: function(message) {
-                return $http.post(PUSH_SERVER_URL + '/messages/', message);
-            }
-        };
-    });
